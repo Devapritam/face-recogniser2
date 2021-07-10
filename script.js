71319523d89bc363541ef6e026d4a794145e1ab8
Devapritam Sharma <dsarma1980@gmail.com>
2021-07-09 19:10:13 -1200

    Add files via upload

--- script.js
+++ script.js
@@ -15,4 +15,4 @@ function captureImg() {
 }
 
 console.log('ml5 version', ml5.version);
-classifier = ml5.imageClassifier('model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/PjCO3vjO2/model.json', modelLoaded);
