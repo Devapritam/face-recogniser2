063da15e547e6279ccf56975411ce770ec78cd71
Devapritam Sharma <dsarma1980@gmail.com>
2021-07-09 19:02:41 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    height: 250,
+    width: 350,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById('camera').innerHTML;
+Webcam.attach('#camera');
+
+function captureImg() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById('resultImg').innerHTML = '<img id="captured_img" src="'+data_uri+'" />';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('model.json', modelLoaded);
