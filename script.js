9192006392b29ab836e8734333050faada747c15
Devapritam Sharma <dsarma1980@gmail.com>
2021-07-12 22:40:00 -1200

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function captureImg() {
 
 console.log('ml5 version', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/PjCO3vjO2/model.json', modelLoaded);
+
+function modelLoaded() {
+    window.alert('Model has been loaded!');
+}
+
+function identifyImg() {
+    image = document.getElementById('captured_img');
+    classifier.classify(image, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById('object_name').innerHTML = results[0].label;
+        document.getElementById('object_accuracy').innerHTML = results[0].confidence.toFixed(3);
+    }
+}
